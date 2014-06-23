3153f36f4b8be58f99bdd3ed62be97f86fa57ff7
Karl Damkjær Hansen <kdh@es.aau.dk>
2014-06-23 10:16:22 +0200

    Provide position control for empty davinci driver.
    Signed-off-by: Karl Damkjær Hansen <kdh@es.aau.dk>

--- src/empty_davinci.cpp
+++ src/empty_davinci.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "controller_manager/controller_manager.h"
 #include "hardware_interface/joint_state_interface.h"
+#include "hardware_interface/joint_command_interface.h"
 #include "hardware_interface/robot_hw.h"
 
 class empty_davinci : public hardware_interface::RobotHW
@@ -10,25 +11,34 @@ public:
         pos[0] = 0.0;
         vel[0] = 0.0;
         eff[0] = 0.0;
+        cmd[0] = 0.0;
 
         // connect and register the joint state interface
         hardware_interface::JointStateHandle state_handle_roll("roll", &pos[0], &vel[0], &eff[0]);
         _joint_state_interface.registerHandle(state_handle_roll);
-
         registerInterface(&_joint_state_interface);
+
+        hardware_interface::JointHandle position_handle_roll(state_handle_roll, &cmd[0]);
+        _position_joint_interface.registerHandle(position_handle_roll);
+        registerInterface(&_position_joint_interface);
     };
 
     ~empty_davinci(){};
 
     void read(){};
-    void write(){};
+    void write()
+    {
+        pos[0] = cmd[0];
+    };
 
 private:
     hardware_interface::JointStateInterface _joint_state_interface;
+    hardware_interface::PositionJointInterface _position_joint_interface;
 
     double pos[1];
     double vel[1];
     double eff[1];
+    double cmd[1];
 };
 
 
