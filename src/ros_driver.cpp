b25d1b703860054f95b98014856341c21a8da34c
Karl Damkjær Hansen <kdh@es.aau.dk>
2014-09-15 15:09:13 +0200

    Clean the structure of the code up a bit.
    Moved the definition of the ros_control specific class
    RosDavinciDriver into the ros_driver.cpp, it seemed cluttery.
    
    I also removed an old broken launch script.

--- src/ros_driver.cpp
+++ src/ros_driver.cpp
@@ -25,10 +25,66 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <utility>
 #include <boost/bind.hpp>
 #include <boost/regex.hpp>
+#include <boost/thread.hpp>
 #include "ros/ros.h"
 #include "ros/console.h"
 #include "controller_manager/controller_manager.h"
 #include "davinci_driver/ros_davinci_driver.h"
+
+RosDavinciDriver::RosDavinciDriver(std::vector<std::pair<std::string, unsigned short> > robot_ips) :
+    _low_level_driver(robot_ips)
+{
+    ROS_INFO("Connecting to Davinci Robot...");
+    boost::thread connect_thread(boost::bind(&DavinciDriver::connect, &_low_level_driver));
+    // Wait for connection or termination
+    while(ros::ok() && connect_thread.joinable())
+    {
+        connect_thread.timed_join(boost::posix_time::seconds(0.1));
+    }
+    // if we were terminated while waiting for connection
+    if (! ros::ok())
+    {
+        connect_thread.interrupt();
+        connect_thread.join();
+        return;
+    }
+    ROS_INFO("Connection to Davinci established.");
+
+    std::vector<std::string> joint_names = _low_level_driver.get_names();
+    for(size_t i = 0; i < joint_names.size(); ++i)
+    {
+        hardware_interface::JointStateHandle state_handle(
+            joint_names[i],
+            &_low_level_driver.joint_positions[i],
+            &_low_level_driver.joint_velocities[i],
+            &_low_level_driver.joint_efforts[i]
+        );
+        _joint_state_interface.registerHandle(state_handle);
+
+        hardware_interface::JointHandle effort_handle(
+            state_handle,
+            &_low_level_driver.joint_efforts[i]
+        );
+        _effort_joint_interface.registerHandle(effort_handle);
+    }
+    registerInterface(&_joint_state_interface);
+    registerInterface(&_effort_joint_interface);
+
+    ROS_INFO("Davinci driver initialized.");
+}
+
+RosDavinciDriver::~RosDavinciDriver()
+{}
+
+void RosDavinciDriver::read()
+{
+    _low_level_driver.read();
+}
+
+void RosDavinciDriver::write()
+{
+    _low_level_driver.write();
+}
 
 
 int main(int argc, char *argv[])
